5520fee4f97afdfd3bbd78013b7dbd8b0a71f167
mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-03-01 20:36:52 -0800

    Chapter 14 examples

---
+++ src/chapter14/StringConstructors.java
@@ -0,0 +1,16 @@
+package chapter14;
+
+public class StringConstructors {
+    public static void main(String[] args) {
+        char[] charArray={'b','i','r','t','h',' ','d','a','y'};
+        String s =new String("hello");
+
+        String s1=new String();
+        String s2=new String(s);
+        String s3=new String(charArray);
+        String s4=new String(charArray,6,3);
+
+        System.out.printf("S1=%s%nS2=%s%nS3=%s%nS4=%s%n",s1
+        ,s2,s3,s4);
+    }
+}
